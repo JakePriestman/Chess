3493fa17d57ac576bd0c69d0f6dcb58158c8ce5f
jake priestman <jakeicus@gmail.com>
2024-04-15 18:46:09 +0200

    added board object

--- Chess/Board.cpp
+++ Chess/Board.cpp
@@ -1,14 +1,45 @@
 #include "Board.h"
 
-void Board::DrawBoard(int width, int height)
+Board::Board(int _squareWidth, int _squareHeight)
+{	
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			Color colour = (j + i) % 2 == 0 ? BEIGE : BROWN;
+
+			squares[j + i * 8] = new Square{ colour, nullptr, _squareWidth, _squareHeight, j, i};
+		}
+	}
+
+	squareHeight = _squareHeight;
+	squareWidth = _squareWidth;
+	piecesTextureSheet = LoadTextureFromImage(LoadImage("Assets/chess_assets.png"));
+}
+
+void Board::DrawBoard()
 {
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 8; j++)
 		{
-			Color colour = (j + i) % 2 == 0 ? WHITE : BLACK;
-
-			DrawRectangle(j * width, i * height, width, height, colour);
+			DrawRectangle(j * squareWidth, i * squareHeight, squareWidth, squareHeight, squares[j+i*8]->colour);
 		}
 	}
+}
+
+void Board::DrawSquare(int x, int y, Color colour)
+{
+	DrawRectangle(x * squareWidth, y * squareHeight, squareWidth, squareHeight, colour);
+}
+
+Piece* Board::GetPiece(int x, int y)
+{
+	return squares[x + 8 * y]->piece;
+}
+
+
+Square* Board::GetSquare(int x, int y)
+{
+	return squares[(x / squareWidth) + (y / squareHeight) * 8];
 }
--- Chess/Board.h
+++ Chess/Board.h
@@ -1,9 +1,31 @@
 #pragma once
 
 #include "raylib.h"
+#include "Piece.h"
+
+//#define SQUARE_HEIGHT CLITERAL (int)80
+//#define SQUARE_WIDTH CLITERAL (int)80
+
 
 class Board
 {
 public:
-	void DrawBoard(int width, int height);
+	Board() = default;
+
+	Board(int squareWidth, int squareHeight);
+
+	void DrawBoard();
+
+	void DrawSquare(int x, int y, Color colour);
+
+	Piece* GetPiece(int x, int y);
+
+	Square* GetSquare(int x, int y);
+
+	Square* squares[64];
+
+	Texture2D piecesTextureSheet;
+
+	int squareWidth, squareHeight;
+
 };
