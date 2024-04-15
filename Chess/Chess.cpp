dd10d0e750a2db7ca90fdecf46379a67b279c4ce
jake priestman <jakeicus@gmail.com>
2024-04-15 18:46:00 +0200

    added chess.cpp

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include "Game.h"
 
-Game* game;
-
 int main()
 {
-    game = new Game();
-    game->run(640, 640, "Chess", 1000);
+    Game game = Game{};
+    game.Run(640, 640, "Chess", 1000);
 
     return 0;
 }
